ad657c2da99397c1408313443d7ec3a6824652d6
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-10-09 15:11:08 -0700

    Finish Contact Disclaimer Component

---
+++ components/ContactDisclaimer.js
@@ -0,0 +1,66 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const StyledContactDisclaimerContainer = styled.div`
+  background-color: ${(props) => props.theme.colors.primary2};
+  background-image: url('/bg-pattern-home-6-about-5.svg');
+  background-repeat: no-repeat;
+  background-position: 0px 115%;
+
+  @media (min-width: 768px) {
+    min-height: 13rem;
+    display: flex;
+    align-items: center;
+    align-content: center;
+  }
+`;
+
+const StyledContactDisclaimerContentWrapper = styled.div`
+  text-align: center;
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 5.1875rem;
+  padding-bottom: 6.25rem;
+
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    padding-top: unset;
+    padding-bottom: unset;
+    justify-content: space-around;
+    max-width: 917px;
+  }
+
+  h2,
+  a {
+    color: ${(props) => props.theme.colors.secondary4};
+  }
+
+  h2 {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 2.5rem;
+
+    @media (min-width: 768px) {
+      margin-bottom: unset;
+    }
+  }
+
+  a {
+    padding: 11px 33px;
+    text-decoration: none;
+    border: 2px solid ${(props) => props.theme.colors.secondary4};
+    border-radius: 24px;
+  }
+`;
+
+export default function ContactDisclaimer() {
+  return (
+    <StyledContactDisclaimerContainer>
+      <StyledContactDisclaimerContentWrapper>
+        <h2>Ready to get started?</h2>
+        <Link href="/contact">Contact Us</Link>
+      </StyledContactDisclaimerContentWrapper>
+    </StyledContactDisclaimerContainer>
+  );
+}
---
+++ components/SuccessStoriesSection.js
@@ -0,0 +1,26 @@
+export default function SuccessStoriesSection() {
+  return (
+    <>
+      <h1>
+        Delivering real results for top companies. Some of our success stories.
+      </h1>
+      <p>
+        “The team perfectly fit the specialized skill set required. They focused
+        on the most essential features helping us launch the platform eight
+        months faster than planned.”
+      </p>
+      <p>Kady Baker</p>
+      <p>Product Manager at Bookmark</p>
+      <img src="/avatar-kady.jpg" alt="" />
+
+      <p>
+        “The team perfectly fit the specialized skill set required. They focused
+        on the most essential features helping us launch the platform eight
+        months faster than planned.”
+      </p>
+      <p>Kady Baker</p>
+      <p>Product Manager at Bookmark</p>
+      <img src="/avatar-kady.jpg" alt="" />
+    </>
+  );
+}
