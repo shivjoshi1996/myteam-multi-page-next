eb91cee8e88f679212eaad7d60f143ecc74d7443
shivjoshi1996 <joshi.k.shiv@gmail.com>
2021-10-12 14:02:23 -0700

    WIP Desktop styling of nav bar

--- components/Footer.js
+++ components/Footer.js
@@ -13,6 +13,78 @@ const StyledFooter = styled.footer`
   }
 `;
 
+const StyledFooterWrapper = styled.div`
+  @media (min-width: 1080px) {
+    display: flex;
+    width: 90%;
+    max-width: 1110px;
+    margin: 0 auto;
+  }
+`;
+
+const StyledFooterTopContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  margin-bottom: 40px;
+
+  @media (min-width: 768px) {
+    width: 90%;
+    margin: 0 auto;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 2rem;
+    align-items: stretch;
+  }
+
+  @media (min-width: 1080px) {
+    margin-bottom: unset;
+    justify-content: unset;
+    gap: 125px;
+  }
+`;
+
+const StyledFooterLogoLinksWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+
+  @media (min-width: 768px) {
+    text-align: left;
+    gap: unset;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+`;
+
+const StyledFooterAddress = styled.address`
+  opacity: 0.6;
+  line-height: 24px;
+
+  @media (min-width: 768px) {
+    text-align: right;
+  }
+
+  @media (min-width: 1080px) {
+    text-align: left;
+  }
+`;
+
+const StyledFooterBottomContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+    width: 90%;
+    margin: 0 auto;
+  }
+`;
+
 const StyledFooterNavigationLinks = styled.ul`
   display: flex;
   justify-content: center;
@@ -24,50 +96,60 @@ const StyledFooterNavigationLinks = styled.ul`
   }
 `;
 
-const StyledFooterAddress = styled.address`
-  opacity: 0.6;
-`;
-
 const StyledFooterSocialLinks = styled.ul`
   display: flex;
   justify-content: center;
+  align-items: center;
   gap: 16px;
+`;
+
+const StyledLogo = styled.img`
+  width: 96px;
+  height: auto;
 `;
 
 export default function Footer() {
   return (
     <StyledFooter>
-      <img src="/logo.svg" alt="myteam logo" />
-      <StyledFooterNavigationLinks>
-        <li>
-          <Link href="/">home</Link>
-        </li>
-        <li>
-          <Link href="/about">about</Link>
-        </li>
-      </StyledFooterNavigationLinks>
-      <StyledFooterAddress>
-        987 Hillcrest Lane
-        <br />
-        Irvine, CA
-        <br />
-        California 92714
-        <br />
-        Call Us : 949-833-7432
-        <br />
-      </StyledFooterAddress>
-      <StyledFooterSocialLinks>
-        <li>
-          <img src="/icon-facebook.svg" alt="" />
-        </li>
-        <li>
-          <img src="/icon-pinterest.svg" alt="" />
-        </li>
-        <li>
-          <img src="/icon-twitter.svg" alt="" />
-        </li>
-      </StyledFooterSocialLinks>
-      <p>Copyright 2020. All Rights Reserved</p>
+      <StyledFooterWrapper>
+        <StyledFooterTopContainer>
+          <StyledFooterLogoLinksWrapper>
+            <StyledLogo src="/logo.svg" alt="myteam logo" />
+            <StyledFooterNavigationLinks>
+              <li>
+                <Link href="/">home</Link>
+              </li>
+              <li>
+                <Link href="/about">about</Link>
+              </li>
+            </StyledFooterNavigationLinks>
+          </StyledFooterLogoLinksWrapper>
+          <StyledFooterAddress>
+            987 Hillcrest Lane
+            <br />
+            Irvine, CA
+            <br />
+            California 92714
+            <br />
+            Call Us : 949-833-7432
+            <br />
+          </StyledFooterAddress>
+        </StyledFooterTopContainer>
+        <StyledFooterBottomContainer>
+          <StyledFooterSocialLinks>
+            <li>
+              <img src="/icon-facebook.svg" alt="" />
+            </li>
+            <li>
+              <img src="/icon-pinterest.svg" alt="" />
+            </li>
+            <li>
+              <img src="/icon-twitter.svg" alt="" />
+            </li>
+          </StyledFooterSocialLinks>
+          <p>Copyright 2020. All Rights Reserved</p>
+        </StyledFooterBottomContainer>
+      </StyledFooterWrapper>
     </StyledFooter>
   );
 }
